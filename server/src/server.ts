b085ac4ec1d686a1d395526b72ead13c78ee5e49
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-01-20 06:59:16 +0200

    feat(server): add initial routes for habits

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,24 @@
 import fastify from "fastify";
+import { title } from "process";
+
+import { prisma } from "@/database/prisma";
 
 const app = fastify();
 const PORT = 3333;
 
-app.get("/hello", () => {
-  return {
-    hello: "world",
-  };
+app.get("/habits", async () => {
+  return prisma.habit.findMany();
+});
+
+app.post<{ Body: { title: string } }>("/habits", async (request, reply) => {
+  const { title } = request.body;
+  if (!title) return reply.code(400).send({ error: "O título é obrigatório" });
+  const habit = await prisma.habit.create({
+    data: {
+      title,
+    },
+  });
+  return habit;
 });
 
 app.listen({ port: PORT }, () => console.log("Server running on port:", PORT));
