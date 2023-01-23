6904d0e2a4a9751efe6ec4702951f0c6091b4f25
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-01-23 11:47:36 +0200

    feat(server): setup cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import "./lib/dayjs";
 
+import cors from "@fastify/cors";
 import fastify from "fastify";
 
 import { setupRoutes } from "./routes";
 
 const app = fastify();
 
+app.register(cors);
 app.register(setupRoutes);
 
 const PORT = 3333;
